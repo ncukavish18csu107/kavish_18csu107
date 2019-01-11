2a0ccb4ac601c06fac6c7b2800d32cbf60f03325
KAVISH AGGARWAL <kavish18csu107@ncuindia.edu>
2019-01-11 11:05:27 +0530

    test

---
+++ FOCP II/Data types/src/first.java
@@ -0,0 +1,18 @@
+class first
+{
+	public static void main(String[]args)
+    {
+      int n1=2 , n2=8;
+      int add , sub , mul , div;
+
+      add = n1 + n2;
+      sub = n1 - n2;
+      mul = n1 * n2;
+      div = n1/n2;
+      
+      System.out.println(add);
+      System.out.println(sub);
+      System.out.println(div);
+      System.out.println(mul);
+    }
+}
